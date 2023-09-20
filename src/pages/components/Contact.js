44155621e41d0d55f2e14680c6546b7ff7329ad6
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-09-20 16:15:57 +0530

    contact fix and footer fix

--- src/pages/components/Contact.js
+++ src/pages/components/Contact.js
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Head from "next/head";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -22,30 +23,38 @@ const Contact = () => {
       <p className="font-jb text-center text-[#b3b3b3] w-11/12 lg:w-2/5">If you have any questions or would like more information about our services, please feel free to contact us. We're here to help!</p>
       
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10 md:pt-10">
+        <Link href="https://www.instagram.com/crowdento" target="_blank">
         <div className="flex flex-col items-center">
-          <img src="/loc.png" alt="Phone" className="w-10 md:w-16 h-auto" />
-          <p className="font-jb text-[#b3b3b3] pt-2">Address</p> 
-          <p className="font-jb text-[#b3b3b3] pt-2 text-center w-11/12">Chennai,Tamilnadu, India.</p>
+          <img src="/insta.png" alt="Phone" className="w-10 md:w-16 h-auto" />
+          <p className="font-jb text-[#b3b3b3] pt-2">Instagram</p> 
+          <p className="font-jb text-[#b3b3b3] pt-2 text-center w-11/12">Crowdento Events</p>
         </div>
+        </Link>
 
+        <Link href="tel:+919840457238" target="_blank">
         <div className="flex flex-col items-center ">
           <img src="/phone.png" alt="Phone" className=" w-10 md:w-16 h-auto" />
           <p className="font-jb text-[#b3b3b3] pt-2">Phone</p> 
           <p className="font-jb text-[#b3b3b3] pt-2 text-center w-11/12">+91 9840457238</p>
         </div>
+        </Link>
 
+        <Link href="mailto:contact@crowdento.com" target="_blank">
         <div className="flex flex-col items-center">
           <img src="/email.png" alt="Phone" className="w-10 md:w-16 h-auto" />
           <p className="font-jb text-[#b3b3b3] pt-2">Email</p>
           <p className="font-jb text-[#b3b3b3] pt-2 text-center w-11/12">contact@crowdento.com</p>
         </div>
-
+        </Link>
+      <Link href="https://wa.me/919840457238" target="_blank">
         <div className="flex flex-col items-center">
           <img src="/wap.png" alt="Phone" className="w-10 md:w-16 h-auto" />
           <p className="font-jb text-[#b3b3b3] pt-2">Whatsapp</p>
           <p className="font-jb text-[#b3b3b3] pt-2 text-center w-11/12">+91 9840457238</p>
         </div>
+        </Link>
       </div>
+      
       
       <div className="w-full bg-white px-2 py-2 items-center flex justify-center">
         <h1 className="text-black font-medium">{`© ${new Date().getFullYear()} Crowdento Events. All Rights Reserved.`}</h1>
